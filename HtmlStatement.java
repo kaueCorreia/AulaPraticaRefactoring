56c7d12d7c0fab443c908e01a4622484af2bb272
kaueCorreia <163582374+kaueCorreia@users.noreply.github.com>
2025-11-26 23:51:35 -0300

    COMMIT & PUSH
    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,14 +1,17 @@
 public class HtmlStatement extends Statement {
 
+    @Override
     protected String headerString(Customer aCustomer) {
         return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
     }
 
+    @Override
     protected String eachRentalString(Rental each) {
         return each.getMovie().getTitle() + ": " +
-                String.valueOf(each.getCharge()) + "<BR>\n";
+                each.getCharge() + "<BR>\n";
     }
 
+    @Override
     protected String footerString(Customer aCustomer) {
         return "<P>You owe <EM>" + aCustomer.getTotalCharge() + "</EM><P>\n" +
                 "On this rental you earned <EM>" +
--- TextStatement.java
+++ TextStatement.java
@@ -1,14 +1,17 @@
 public class TextStatement extends Statement {
 
+    @Override
     protected String headerString(Customer aCustomer) {
         return "Rental Record for " + aCustomer.getName() + "\n";
     }
 
+    @Override
     protected String eachRentalString(Rental each) {
         return "\t" + each.getMovie().getTitle() + "\t" +
-                String.valueOf(each.getCharge()) + "\n";
+                each.getCharge() + "\n";
     }
 
+    @Override
     protected String footerString(Customer aCustomer) {
         return "Amount owed is " + aCustomer.getTotalCharge() + "\n" +
                 "You earned " + aCustomer.getTotalFrequentRenterPoints() +
